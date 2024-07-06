d17b1bc7545747b20b6beb7c582a89bac76f500a
abhishektiwari28 <abhishektiwari2807@gmail.com>
2024-07-06 10:11:37 +0530

    2.1 change - Ui Design Complete

--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -10,3 +10,18 @@ const LogoutButton = () => {
 }
 
 export default LogoutButton
+
+
+
+// import React from 'react'
+// import { BiLogOut } from 'react-icons/bi'
+
+// const LogoutButton = () => {
+//   return (
+//     <div className='mt-auto '>
+//         <BiLogOut className='w-6 h-6 text-white cursor-pointer' />
+//     </div>
+//   )
+// }
+
+// export default LogoutButton
